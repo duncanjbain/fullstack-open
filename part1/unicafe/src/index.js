18d793200d736baf5479f39cd6c4b03ff804bbe4
Duncan Bain <duncan@bain.io>
2020-07-28 21:26:46 +0100

    unicafe part1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Button = ({ handleClick, buttonText}) => (
+  <button onClick={handleClick}>{buttonText}</button>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Feedback = ({ ratingType, ratingTotal }) => (
+  <div>
+    <p>{ratingType} : {ratingTotal}</p>
+  </div>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good+1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral+1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad+1);
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>Give Feedback</h2>
+        <Button buttonText="Good" handleClick={handleGoodClick} />
+        <Button buttonText="Neutral" handleClick={handleNeutralClick} />
+        <Button buttonText="Bad" handleClick={handleBadClick} />
+      </div>
+      <div>
+        <h2>Statisics</h2>
+        <div>
+          <Feedback ratingType="Good" ratingTotal={good} />
+          <Feedback ratingType="Neutral" ratingTotal={neutral} />
+          <Feedback ratingType="Bad" ratingTotal={bad} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
