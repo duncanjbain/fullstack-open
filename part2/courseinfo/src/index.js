e335cec2b25e6c61b1e180aff19665e88c82b562
Duncan Bain <duncan@bain.io>
2020-08-05 20:40:22 +0100

    part2 courseinfo step 6

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -5,58 +5,57 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
-const Part = (props) => {
+const Part = ({course, exercises, id}) => {
   return (
-    <p>
-      {props.course} {props.exercises}
+    <p key={id}>
+      {course} {exercises}
     </p>
   );
 };
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part course={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part course={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part course={props.parts[2].name} exercises={props.parts[2].exercises} />
+      {parts.map(part => 
+      <Part course={part.name} exercises={part.exercises} id={parts.id} />
+      )}
     </div>
   );
 };
 
-const Total = (props) => {
+const Course = ({course}) => {
   return (
-    <p>
-      Number of exercises is {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  );
-};
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </>
+  )
+}
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+  return <Course course={course} />
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
