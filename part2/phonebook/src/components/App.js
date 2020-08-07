5fe19db8877b4708f801261a57db0735d7e70515
Duncan Bain <duncan@bain.io>
2020-08-07 18:01:28 +0100

    phonebook part6

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import AddPersonForm from "./AddPersonForm";
 import Search from "./Search";
 import DisplayPersons from "./DisplayPersons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhoneNumber, setNewPhoneNumber] = useState("");
   const [nameSearch, setNewNameSearch] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(response => (
+      setPersons(response.data)
+    ))
+  })
 
   const personsToShow = persons.filter((person) =>
     person.name.toLowerCase().includes(nameSearch.toLowerCase())
