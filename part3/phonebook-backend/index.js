341d8131e2f864829420ea2ffaa246d0f9b2002a
Duncan Bain <duncan@bain.io>
2020-08-12 17:33:27 +0100

    update key and baseurl

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -18,22 +18,22 @@ let persons =[
   { 
     "name": "Arto Hellas", 
     "number": "040-123456",
-    "id": 1
+    "id": "1"
   },
   { 
     "name": "Ada Lovelace", 
     "number": "39-44-5323523",
-    "id": 2
+    "id": "2"
   },
   { 
     "name": "Dan Abramov", 
     "number": "12-43-234345",
-    "id": 3
+    "id": "3"
   },
   { 
     "name": "Mary Poppendieck", 
     "number": "39-23-6423122",
-    "id": 4
+    "id": "4"
   }
 ]
 
@@ -43,7 +43,7 @@ app.get('/api/persons/', (req, res) => {
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
+  const id = req.params.id
   const person = persons.find(person => person.id === id)
   if(person) {
   return res.json(person).status(200)
