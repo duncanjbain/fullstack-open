2fef563ef8e34b097c08412f62ed367191a2635e
Duncan Bain <duncan@bain.io>
2020-08-15 18:31:31 +0100

    part 4 step 3 test complete

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,12 +1,26 @@
 const dummy = (blogs) => {
-return 1;
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-  const sum = blogs.map(blog => blog.likes).reduce((prev, curr) => prev+curr, 0)
-  return sum;
-}
+  return blogs.map((blog) => blog.likes).reduce((prev, curr) => prev + curr, 0);
+};
+
+const blogWithMostLikes = (blogs) => {
+  const mostLikes = blogs
+    .map((blog) => blog.likes)
+    .reduce((prev, curr) => Math.max(prev, curr));
+  const mostLikedBlog = blogs.find((blog) => blog.likes === mostLikes);
+
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  };
+};
 
 module.exports = {
-  dummy, totalLikes
-}
+  dummy,
+  totalLikes,
+  blogWithMostLikes,
+};
