9682994c06277454dc358e3fea18b661f9173241
Duncan Bain <duncan@bain.io>
2020-08-23 17:19:20 +0100

    5.15 step3

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,41 +1,41 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent } from '@testing-library/react'
-import Blog from './Blog'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent } from "@testing-library/react";
+import Blog from "./Blog";
 
+describe("blog viewing", () => {
+  const blog = {
+    title: "Jest Test Blog",
+    author: "Jest Test Author",
+    url: "google.com",
+    likes: 15,
+  };
 
-const blog = {
-  title: "Jest Test Blog",
-  author: "Jest Test Author",
-  url: "google.com",
-  likes: 15
-}
+  test("renders blog title and author only by default", () => {
+    const  component = render(<Blog blog={blog} />);
+    expect(component.container).toHaveTextContent("Blog: Jest Test Blog");
+    expect(component.container).toHaveTextContent("Author: Jest Test Author");
+    expect(component.container).not.toHaveTextContent("Blog Url: google.com");
+    expect(component.container).not.toHaveTextContent("Total Likes: 15");
+  });
 
-test('renders blog title and author only by default', () => {
- 
+  test("renders blog url and blog likes when view button is clicked", () => {
+    const  component = render(<Blog blog={blog} />);
+    const viewButton = component.getByText("View");
+    fireEvent.click(viewButton);
+    expect(component.container).toHaveTextContent("Blog: Jest Test Blog");
+    expect(component.container).toHaveTextContent("Author: Jest Test Author");
+    expect(component.container).toHaveTextContent("Blog URL: google.com");
+    expect(component.container).toHaveTextContent("Total Likes: 15");
+  });
 
-  const component = render(
-    <Blog blog={blog} />
-  )
-  
-
-  expect(component.container).toHaveTextContent('Blog: Jest Test Blog')
-  expect(component.container).toHaveTextContent('Author: Jest Test Author')
-  expect(component.container).not.toHaveTextContent('Blog Url: google.com')
-  expect(component.container).not.toHaveTextContent('Total Likes: 15')
-})
-
-test('renders blog url and blog likes when view button is clicked', () => {
-  const component = render(
-    <Blog blog={blog} />
-  )
-
-  const button = component.getByText('View')
-  fireEvent.click(button)
-  expect(component.container).toHaveTextContent('Blog: Jest Test Blog')
-  expect(component.container).toHaveTextContent('Author: Jest Test Author')
-  expect(component.container).toHaveTextContent('Blog URL: google.com')
-  expect(component.container).toHaveTextContent('Total Likes: 15')
-
-})
-
+  test("if the like button is clicked twice, the event handler for adding likes is called twice", () => {
+    const mockHandler = jest.fn();
+    const component = render(<Blog blog={blog} handleLike={mockHandler} />);
+    const viewButton = component.getByText("View");
+    fireEvent.click(viewButton);
+    const likeButton = component.getByText("Like!");
+    fireEvent.click(likeButton);
+    expect(mockHandler.mock.calls).toHaveLength(1);
+  });
+});
