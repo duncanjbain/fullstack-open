f1e3b7d9b776156b19796c69ff340daba1a516fc
Duncan Bain <duncan@bain.io>
2020-08-23 17:02:04 +0100

    5.14 step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
+
+const blog = {
+  title: "Jest Test Blog",
+  author: "Jest Test Author",
+  url: "google.com",
+  likes: 15
+}
+
 test('renders blog title and author only by default', () => {
-  const blog = {
-    title: "Jest Test Blog",
-    author: "Jest Test Author",
-    url: "google.com",
-    likes: 15
-  }
+ 
 
   const component = render(
     <Blog blog={blog} />
@@ -20,5 +23,19 @@ test('renders blog title and author only by default', () => {
   expect(component.container).toHaveTextContent('Author: Jest Test Author')
   expect(component.container).not.toHaveTextContent('Blog Url: google.com')
   expect(component.container).not.toHaveTextContent('Total Likes: 15')
+})
+
+test('renders blog url and blog likes when view button is clicked', () => {
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+  expect(component.container).toHaveTextContent('Blog: Jest Test Blog')
+  expect(component.container).toHaveTextContent('Author: Jest Test Author')
+  expect(component.container).toHaveTextContent('Blog URL: google.com')
+  expect(component.container).toHaveTextContent('Total Likes: 15')
+
 })
 
