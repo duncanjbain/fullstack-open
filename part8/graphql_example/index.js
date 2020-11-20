a64c600bd5719331f8b26b5e0d69d3499802f388
Duncan Bain <duncan@bain.io>
2020-11-20 20:55:00 +0000

    part 8 step 8.3

--- part8/graphql_example/index.js
+++ part8/graphql_example/index.js
@@ -91,18 +91,28 @@ const typeDefs = gql`
       id: ID!
       genres: [String!]
   }
-  type Query {
+  type Author {
+      name: String!
+      id: ID!
       bookCount: Int!
+  }
+  type Query {
       authorCount: Int!
-      allBooks: [Book!]
+      allBooks: [Book!]!
+      allAuthors: [Author!]!
+      bookCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-      bookCount: () => books.length,
       authorCount: () => authors.length,
+      bookCount: () => books.length,
       allBooks: () => books,
+      allAuthors: () => authors,
+  },
+  Author: {
+      bookCount: (root, args) => books.filter((book) => (book.author == root.name)).length
   }
 }
 
